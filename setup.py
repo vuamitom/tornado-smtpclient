75985b0e74be882bb2a2cca11719fa525e90d41a
Tam <vumhtam@gmail.com>
2014-07-28 22:24:44 +0700

    add setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tornado-smtpclient',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/vuamitom/tornado-smtpclient',
+    license='',
+    author='tamvu',
+    author_email='vumhtam@gmail.com',
+    description='A non-blocking smtp client to work with tornado-based application'
+)
