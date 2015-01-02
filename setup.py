c95a19a68a1216453ccfcf91ff4383c66f7c27f8
Stefano Palazzo <stefano.palazzo@gmail.com>
2015-01-02 16:01:00 +0100

    trove classifier

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'Topic :: Communications :: Email :: Email Clients (MUA)',
 
         # Pick your license as you wish (should match "license" above)
-        # 'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
